1880a8a0f8042f188fde67e555195ebcefb83577
PRATHAP KUDUPU <PRATHAP KUDUPU>
2017-03-03 17:48:11 -0800

    Rest Api to fetch specific user has been added

--- app.js
+++ app.js
@@ -32,7 +32,7 @@ mongoose.connect('mongodb://127.0.0.1:27017/Ecommerce');
 //log connection state
 console.log(mongoose.connection.readyState);
 //database Object
-var db= mongoose.connection;
+//var db= mongoose.connection;
 //Genre schema. we can do validation
 var companySchema=mongoose.Schema({
  companyName:{
@@ -46,18 +46,57 @@ var companySchema=mongoose.Schema({
 
 //Creating a model using its schema
 var Company= mongoose.model("companys",companySchema)
-//Using find to fetch the collection
 
+//Using find to fetch the collection
 app.get('/api/Companys',function(req,res){
     Company.find({},function(req,companys){
     console.log(companys);
     //sends the response to the browser
     res.send(companys);
-   
-    
 });
+});
+//Model for fetching users
+var userSchema=mongoose.Schema({
+    firstName:{
+      type:String,
+      required:true
+    },
+    lastName:{
+      type:String
+      
+    }
+});
+//User Model
+var User=mongoose.model("users",userSchema)
+
+//Using find to fetch all the users in the db
+app.get("/api/Users",function(req,res){
+  User.find({},function (err,user){
+    if(err)
+    {
+       console.log(err);
+       res.send(err);
+    }
+    else
+   {
+     console.log(user);
+    res.send(user);}
+  });
 
 });
+
+//Using find to fetch all the users in the db
+app.get("/api/Users/:_id",function(req,res){
+      User.findById(req.params._id, function(err, bear) {
+            if (err)
+                res.send(err);
+            res.json(bear);
+        });
+  });
+ 
+
+//Fetch specific user
+
 var port =5000;
 //Specify a port to listen for express
 app.listen(port,function(err){
